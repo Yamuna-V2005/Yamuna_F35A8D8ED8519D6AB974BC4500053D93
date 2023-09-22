b1a5ae47e18487efa84ace356f7dcbfb91bdadfa
Yamuna-V2005 <144877105+Yamuna-V2005@users.noreply.github.com>
2023-09-22 12:47:35 +0530

    Add files via upload

--- main (3).py
+++ main (3).py
@@ -1,15 +1,29 @@
-# Leap year
+'''Implement a class called Player that represents cricket player.The Player class should have a
+method called play() which prints "The Player is playing cricket. Derive two classes, Batsman and
+Bowler, from the Player class. Override the play() method in each derived class to print "The batsman
+is batting" and " The Bowler is bowling", respectively. Write a program to create objects of both the 
+Batsman and Bowler classes and call the play() method for each object.'''
 
 
-def isLeapyear(year):
-  if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
-    return True
-  else:
-    return False
+#Define the base class Player 
+class Player:
+    def play(self):
+      print("The Player is playing cricket.")
 
+#Define the derived class Batsman 
+class Batsman(Player):
+    def play(self):
+      print("The Batsman is batting.")
 
-year = int(input("Enter a year: "))
-if isLeapyear(year):
-  print('{} is a Leap year.'.format(year))
-else:
-  print('{} is not a Leap year.'.format(year))
+#Define the derived class Bowler
+class Bowler(Player):
+    def play(self):
+      print("The Bowler is bowling.")
+      
+#Create objects of Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
+
+#Call the play() method for each object 
+batsman.play()
+bowler.play()
